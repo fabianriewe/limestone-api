61d399da340338353c2d92e36b9c03716db39b49
Fabian Riewe <fabiontop@gmail.com>
2020-12-28 20:25:34 +0100

    feat: added getPriceAtBlock functionality

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-const { run } = require("ar-gql");
+const {run} = require("ar-gql");
 const Arweave = require("arweave/node");
 
 const VERSION = "0.005";
 
 const client = Arweave.init({
-  host: "arweave.net",
-  port: 443,
-  protocol: "https",
+    host: "arweave.net",
+    port: 443,
+    protocol: "https",
 });
 
 async function findGraphQL(parameters) {
-  const res = (
-    await run(
-      `
+    console.log("I am here")
+    console.log(parameters)
+    const res = (
+        await run(
+            `
     {
       transactions(
         tags: [
@@ -21,9 +23,7 @@ async function findGraphQL(parameters) {
           { name: "token", values: "${parameters.token}" }
           { name: "version", values: "${VERSION}" }
         ]
-        block: { min: ${
-          parseInt((await client.network.getInfo()).height) - 50
-        } }
+        block: { min: ${parameters.atBlock} }
         first: 1
       ) {
         edges {
@@ -37,34 +37,51 @@ async function findGraphQL(parameters) {
       }
     }
     `
-    )
-  ).data.transactions.edges;
-
-  if (res[0]) {
-    let tags = res[0].node.tags;
-    let result = {};
-    tags.forEach((tag) => {
-      if (tag.name === "value") {
-        result.price = parseFloat(tag.value);
-      }
-      if (tag.name === "time") {
-        result.updated = new Date(parseInt(tag.value));
-      }
-    });
-    return result;
-  } else {
-    throw Error("Invalid data returned from Arweave.");
-  }
+        )
+    ).data.transactions.edges;
+    
+    if (res[0]) {
+        let tags = res[0].node.tags;
+        let result = {};
+        tags.forEach((tag) => {
+            if (tag.name === "value") {
+                result.price = parseFloat(tag.value);
+            }
+            if (tag.name === "time") {
+                result.updated = new Date(parseInt(tag.value));
+            }
+        });
+        return result;
+    } else {
+        throw Error("Invalid data returned from Arweave.");
+    }
 }
 
 module.exports = {
-  getPrice: async function (token) {
-    if (typeof token !== "string")
-      throw new TypeError("Please provide a token symbol as string.");
+    getPrice: async function (token) {
+        if (typeof token !== "string")
+            throw new TypeError("Please provide a token symbol as string.");
 
-    return await await findGraphQL({
-      type: "data-latest",
-      token,
-    });
-  },
+        // get latest block
+        const latestBlock = parseInt((await client.network.getInfo()).height) - 50
+
+        return await findGraphQL({
+            type: "data-latest",
+            token: token,
+            atBlock: latestBlock
+        });
+    },
+    getPriceAtBlock: async function (token, block) {
+        if (typeof token !== "string")
+            throw new TypeError("Please provide a token symbol as string.");
+
+        if (typeof block !== "number")
+            throw new TypeError("Please provide a token symbol as number.");
+
+        return await findGraphQL({
+            type: "data-latest",
+            token: token,
+            atBlock: block
+        });
+    },
 };
